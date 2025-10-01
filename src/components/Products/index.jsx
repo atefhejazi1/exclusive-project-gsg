5989b6d8a16d1f4034a307e8b9f436f09394e116
Atef Hejazi <78237128+atefhejazi1@users.noreply.github.com>
2025-10-01 09:04:25 +0300

    making add to cart btn

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -7,6 +7,7 @@ const products = [
     originalPrice: 360,
     discountPrice: 260,
     rate: 5,
+    isNew: true,
   },
   {
     image: Slide1Img1,
@@ -14,6 +15,7 @@ const products = [
     originalPrice: 360,
     discountPrice: 260,
     rate: 4,
+    isNew: false,
   },
   {
     image: Slide1Img1,
@@ -21,6 +23,7 @@ const products = [
     originalPrice: 360,
     discountPrice: 260,
     rate: 3,
+    isNew: false,
   },
   {
     image: Slide1Img1,
@@ -28,6 +31,7 @@ const products = [
     originalPrice: 360,
     discountPrice: 260,
     rate: 5,
+    isNew: true,
   },
 ];
 
@@ -59,12 +63,70 @@ const Index = () => {
         {products.map((product, index) => {
           return (
             <div key={index} className="flex flex-col">
-              <div className="flex justify-center items-center h-64 bg-[#f5f5f5] rounded-lg">
+              <div className="relative flex justify-center items-center h-64 bg-[#f5f5f5] rounded-lg group overflow-hidden">
+                {/* الصورة */}
                 <img
                   src={product.image}
                   alt="Product"
                   className="max-h-full max-w-full object-contain"
                 />
+
+               {product.isNew &&  <div className="absolute top-2 left-2 p-1 rounded bg-green-400  text-white">
+                  New
+                </div>}
+
+                {/* زر Add to Cart */}
+                <button className="absolute cursor-pointer bottom-0 translate-y-full group-hover:translate-y-0 bg-black w-full text-white py-2 px-4 rounded transition-transform">
+                  Add to Cart
+                </button>
+
+                {/* الأيقونات */}
+                <div className="absolute top-2 right-2 flex flex-col gap-2">
+                  {/* أيقونة 1 */}
+                  <button className="bg-white p-2 rounded-full cursor-pointer">
+                    <svg
+                      width="18"
+                      height="16"
+                      viewBox="0 0 18 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5 1C2.7912 1 1 2.73964 1 4.88594C1 6.61852 1.7 10.7305 8.5904 14.8873C8.71383 14.961 8.85552 15 9 15C9.14448 15 9.28617 14.961 9.4096 14.8873C16.3 10.7305 17 6.61852 17 4.88594C17 2.73964 15.2088 1 13 1C10.7912 1 9 3.35511 9 3.35511C9 3.35511 7.2088 1 5 1Z"
+                        stroke="black"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+
+                  {/* أيقونة 2 */}
+                  <button className="bg-white p-2 rounded-full cursor-pointer">
+                    <svg
+                      width="18"
+                      height="16"
+                      viewBox="0 0 22 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20.257 6.962C20.731 7.582 20.731 8.419 20.257 9.038C18.764 10.987 15.182 15 11 15C6.81801 15 3.23601 10.987 1.74301 9.038C1.51239 8.74113 1.38721 8.37592 1.38721 8C1.38721 7.62408 1.51239 7.25887 1.74301 6.962C3.23601 5.013 6.81801 1 11 1C15.182 1 18.764 5.013 20.257 6.962V6.962Z"
+                        stroke="black"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M11 11C12.6569 11 14 9.65685 14 8C14 6.34315 12.6569 5 11 5C9.34315 5 8 6.34315 8 8C8 9.65685 9.34315 11 11 11Z"
+                        stroke="black"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
 
               <h2 className="text-lg font-semibold mt-4">{product.title}</h2>
@@ -73,9 +135,7 @@ const Index = () => {
                 <span className="text-red-500 line-through">
                   ${product.originalPrice}
                 </span>
-                <span className="text-gray-500  line-through">
-                  ${product.discountPrice}
-                </span>
+                <span className="text-gray-500">${product.discountPrice}</span>
               </div>
 
               <div className="flex mt-3">
